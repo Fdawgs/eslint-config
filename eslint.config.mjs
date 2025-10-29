4983e5ff948615101ecadb43da99c0da1738def8
Frazer Smith <frazer.dev@icloud.com>
2025-10-29 15:33:38 +0000

    build: bump deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
 const config = defineConfig([
 	// Include ignore file to prevent linting of files in .gitignore
-	includeIgnoreFile(gitignorePath),
+	includeIgnoreFile(gitignorePath, ".gitignore"),
 	{
 		files: ["**/*.js", "**/*.jsx"],
 		extends: [fdawgs],
